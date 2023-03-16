5cba57d67646e297f42cae970e8226164cde06f6
JoonasKallio <joonaskallio153@gmail.com>
2023-03-16 13:42:13 +0200

    Laskimen korjaus ja taustaväri

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 function laskesumma() {
-    let numb1 = Number(document.getElementById("numero1").value);
-    let numb2= Number(document.getElementById("Numero2").value);
-    let sum = (numb1 + numb2);
-    document.getElementById("vastaus").innerHTML= sum;
+    let numb1 = Number(document.getElementById("num1").value);
+    let numb2= Number(document.getElementById("num2").value);
+    let sum = numb1 + numb2;
+    document.getElementById("vastaus").innerHTML= "Numeroiden summa on: " + sum;
 }
